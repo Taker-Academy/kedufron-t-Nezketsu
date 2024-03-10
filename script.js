57273e58cbee5d21728ee72279d2b1970bf64c45
nezketsu <gregoire.caseaux@epitech.eu>
2024-03-10 16:50:51 +0100

    le site

--- script.js
+++ script.js
@@ -2,38 +2,79 @@
 //   baseURL: 'https://api.kedufront.juniortaker.com/item'
 // });
 
+async function call_api() {
+  try {
+      const response = await axios.get('https://api.kedufront.juniortaker.com/item/');
+      const items = response.data;
+      console.log(items);
+      return items;
+  } catch (error) {
+      console.error(error);
+  }
+}
 
-axios.get('https://api.kedufront.juniortaker.com/item/')
+function pageItems(id) {
+  axios.get('https://api.kedufront.juniortaker.com/item/')
     .then(function (response) {
       const items = response.data;
-      displayItems(items);
-      // GetImage(item.image);
-      console.log(data);
+      const item = items[id];
+      const container = document.getElementById('product-page-' + id)
+      const productPage = document.createElement('div');
+      productPage.className = 'product';
+      productPage.innerHTML = `
+          <div id="image-container-${item.image}"></div>
+          <div class="item-info">
+            <h1>${item.name}</h1>
+            <p class="item-price">Prix: ${item.price}</p>
+            <button class="add-cart">ajouter au panier</button>
+            <p class="item-description">${item.description}</p>
+          </div>
+      `;
+      container.appendChild(productPage);
+      GetImage(item.image);
       })
-      .catch(function (error) {
-      console.error(error);
-  });
+    }
 
-
-  function displayItems(items) {
+async function displayItems() {
+    const items = await call_api()
+    console.log(items);
     const container = document.getElementById('items-container'); // Assurez-vous que cet ID existe dans votre HTML
-    container.innerHTML = '';
-
     items.forEach(item => {
         if (item._id == 1 || item._id == 3 || item._id == 4 || item._id == 6) {
           const itemDiv = document.createElement('div');
           itemDiv.className = 'item';
           itemDiv.innerHTML = `
-              <div id="image-container-${item.image}"></div>
-              <h1>${item.name}</h1>
-              <p>Price: ${item.price}</p>
+              <a href="./${item.name}.html">
+                <div id="image-container-${item.image}"></div>
+                <h1>${item.name}</h1>
+                <p>Prix: ${item.price}</p>
+              </a>
           `;
           container.appendChild(itemDiv);
-          console.log(item.image);
           GetImage(item.image);
         }
     });
 }
+
+async function displayAllItems() {
+  const items = await call_api()
+  console.log(items);
+  const container = document.getElementById('items-container'); // Assurez-vous que cet ID existe dans votre HTML
+  items.forEach(item => {
+        const itemDiv = document.createElement('div');
+        itemDiv.className = 'item';
+        itemDiv.innerHTML = `
+            <a href="./${item.name}.html">
+              <div id="image-container-${item.image}"></div>
+              <h1>${item.name}</h1>
+              <p>Prix: ${item.price}</p>
+            </a>
+        `;
+        container.appendChild(itemDiv);
+        GetImage(item.image);
+      })
+}
+
 
 function GetImage(id) {
   axios.get(`https://api.kedufront.juniortaker.com/item/picture/${id}`, {
